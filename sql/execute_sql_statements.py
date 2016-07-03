a4340280b607cc939d21d965ea2d621e03f22572
shashwat <shashwat195@gmail.com>
2016-07-04 02:40:22 +0530

    removing  mysql-connector-python==2.0.4

--- sql/execute_sql_statements.py
+++ sql/execute_sql_statements.py
@@ -1,8 +1,6 @@
 import os
 
 curr_dir_listing = os.listdir(".")
-print curr_dir_listing
-os.system("mysql -uroot -e \"drop database test_ecommerce_db\" ")
 for files in sorted(curr_dir_listing):
     if not files.startswith("exe"):
         print "current file = %s" % files
